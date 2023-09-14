3bfb6e5843a4ef95ab879566fc2da375bd324afe
panchi <pancho@gmail.com>
2023-09-14 19:14:36 -0300

    integramos app a server

--- server-ia/server.js
+++ server-ia/server.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const app=express();
 const http = require('http');
-const server = http.createServer();
 
 const dotenv = require('dotenv').config(); 
 
+app.use(express.static(publicPath));
+const server = http.createServer(app);
+
 const socketIO = require('socket.io');
+
 const io = socketIO(server);
 
 const path =require('path');
 const { error } = require('console');
 const publicPath = path.resolve(__dirname,'public');
-app.use(express.static(publicPath));
 
 
 io.on('connection', socket => {
